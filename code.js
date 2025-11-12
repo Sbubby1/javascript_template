8b55113e524c2d30e3171850ba1d3007797ce755
Sbubby1 <jcorreia1@email.neit.edu>
2025-11-12 11:15:52 -0500

    P A I N

--- code.js
+++ code.js
@@ -1,4 +1,20 @@
+import { Toolbox } from "./toolbox.js";
+
 let canvas = document.getElementById("myCanvas");
-let pencil = canvas.getContext("2d"); 
+let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
+let toolbox = new Toolbox();
+
+// let myFavoriteLetters = ["a", "b", "c"];
 
 
+// let counts = [0,0,0];
+// for(let i = 0; i < 100; i++) {
+// //     let randomIndex = getRandomIndex(myFavoriteLetters);
+// //     counts[randomIndex] += 1;
+
+//     console.log(toolbox.getRandomItem(myFavoriteLetters));
+// }
+
+
+// let shuffled = toolbox.shuffleArray(myFavoriteLetters);
+// console.log(shuffled);
